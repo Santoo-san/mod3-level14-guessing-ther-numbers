d6865f5736f013e0b39bc72d533bdb4c3a43e2bd
Santoo-san <67684816+Santoo-san@users.noreply.github.com>
2020-09-09 13:41:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,42 +3,44 @@ console.log("console, check");
 let userName = prompt("Welkom! Wat is je naam?", "");
 alert("Hey " + userName);
 
-let randomNumber = 5;
-console.log(randomNumber);
-// function (max) {
-//   return Math.floor(Math.random() * Math.floor(max));
-// };
+let randomNumber = function (max) {
+  //deze werkt dus niet
+  return Math.floor(Math.random() * Math.floor(max));
+};
+console.log(randomNumber(26));
 
-//console.log(randomNumber(26));
+// //dit werkt wel:
+// 5;
+// console.log(randomNumber);
 
-function numberInput() {
-  let num = prompt(
-    "Voer een nummer in van 0 tot 25 om te beginnen met raden...",
-    ""
-  );
+//werkende code:
 
-  if (randomNumber != parseInt(num)) {
-    alert("mis");
-    numberInput();
-  } else {
-    alert("Gefeliciteerd je hebt gewonnen. Het spel is nu af");
-  }
-}
+let numberInput = prompt(
+  "Voer een nummer in van 0 tot 25 om te beginnen met raden...",
+  ""
+);
+console.log(numberInput);
+
+// expected output: a number between 0 and 25
+
+if (parseInt(randomNumber) != parseInt(numberInput)) {
+  alert("mis");
+  prompt();
+} else alert("Gefeliciteerd je hebt gewonnen. Het spel is nu af");
+
+// //poging tot aanpassing
+// function numberInput() {
+//   let num = prompt(
+//     "Voer een nummer in van 0 tot 25 om te beginnen met raden...",
+//     ""
+//   );
+
+//   if (randomNumber != parseInt(num)) {
+//     alert("mis");
+//     numberInput();
+//   } else {
+//     alert("Gefeliciteerd je hebt gewonnen. Het spel is nu af");
+//   }
+// }
 
 alert("Dag " + userName + ". Tot de volgende keer");
-
-//reserve code
-
-// let numberInput = prompt(
-//   "Voer een nummer in van 0 tot 25 om te beginnen met raden...",
-//   ""
-// );
-// console.log(numberInput);
-
-// // expected output: a number between 0 and 25
-
-// if (parseInt(randomNumber) != parseInt(numberInput)) {
-//   alert("mis");
-//   prompt();
-// } else alert("Gefeliciteerd je hebt gewonnen. Het spel is nu af")
-//
